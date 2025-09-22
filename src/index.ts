458ec9c5ed9ed0aaa62caa9f252ae0438b5c6c2c
Александр Васянькин <henchfiend@yandex.ru>
2025-09-22 18:52:47 +0300

    создал файловую структуру проекта

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response } from 'express';
 // Слушаем 3001 порт
 const { PORT = 3001 } = process.env;
 
 const app = express();
+
+app.get('/', (req: Request, res: Response) => {})
 
 app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
